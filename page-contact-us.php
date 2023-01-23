3fa6a163e12d6847b7f71f9d995ae5a93f67743c
Nishit Kumar <nishitkumar@Nishits-MacBook-Air.local>
2023-01-23 21:26:33 +0530

    fix: certain coding and security issues

--- page-contact-us.php
+++ page-contact-us.php
@@ -99,32 +99,10 @@ get_header();
 
 						</div>
 
-<<<<<<< HEAD
 				<?php endwhile; ?>
 			<?php endif; ?>
 		</section>
 		<!--Section: Contact v.2-->
-=======
-                    echo ''
-                        . '<div class="row g-5">'
-                            . '<div class="col-md-6 mb-5" style="background-color: #198754;">'
-                                . $content
-                            .'</div>'
-                            . '<div class="col-md-6 pb-4">'
-                                . '<p></p>'
-                                . '<div class="ratio ratio-16x9">'
-                                   . '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.955467556482!2d88.36194731428297!3d22.505853941090546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027128402c3f61%3A0xcbc6db027edfc17a!2s172%2FA%2C%20Jodhpur%20Park%2C%20Kolkata%2C%20West%20Bengal%20700068!5e0!3m2!1sen!2sin!4v1639562120493!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'
-                                . '</div>'
-                            . '</div>'
-
-                        . '</div>';
-
-                } //End While
-            }    //End If
-        ?>
-        </section>
-        <!--Section: Contact v.2-->
->>>>>>> 2ee3d4b5c84583f7bc5574efaf44f561b246726b
 	</main><!-- #main -->
 
 
